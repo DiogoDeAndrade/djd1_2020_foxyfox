2427ee4ad1f9b3ec5bba7abaf5ee5fc51225ea4a
Diogo de Andrade <diogo.g.andrade@gmail.com>
2021-04-15 12:50:31 +0100

    Aula 2021/04/15

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] float     followSpeed = 0.1f;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPos = transform.position;
+
+        if (followTarget != null)
+        {
+            Vector3 targetPos = followTarget.position;
+            Vector3 error = targetPos - currentPos;
+
+            Vector3 newPos = currentPos + error * followSpeed;
+
+            currentPos = new Vector3(newPos.x, newPos.y, currentPos.z);
+        }
+
+        transform.position = currentPos;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float       moveSpeed = 20.0f;
+    [SerializeField]
+    private float       jumpSpeed = 100.0f;
+    [SerializeField]
+    private int         maxJumps = 1;
+    [SerializeField]
+    private Transform   groundCheckObject;
+    [SerializeField]
+    private float       groundCheckRadius = 3.0f;
+    [SerializeField]
+    private LayerMask   groundCheckLayer;
+
+    private float       hAxis;
+    private Rigidbody2D rb;
+    private int         nJumps;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(hAxis * moveSpeed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+
+        Collider2D collider = Physics2D.OverlapCircle(groundCheckObject.position, groundCheckRadius, groundCheckLayer);
+
+        bool isGround = (collider != null);
+        
+        if ((isGround) && (Mathf.Abs(rb.velocity.y) < 0.1f))
+        {
+            nJumps = maxJumps;
+        }
+
+        if ((Input.GetButtonDown("Jump")) && (nJumps > 0))
+        {
+            Vector2 currentVelocity = rb.velocity;
+
+            currentVelocity.y = jumpSpeed;
+
+            rb.velocity = currentVelocity;
+
+            nJumps--;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheckObject != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(groundCheckObject.position, groundCheckRadius);
+        }
+
+        if (rb != null)
+        {
+            Vector3 velocity = rb.velocity;
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(transform.position + Vector3.up * 10.0f, transform.position + velocity + Vector3.up * 10.0f);
+        }
+    }
+}
