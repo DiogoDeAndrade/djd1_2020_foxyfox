4ac4b3da31bcc84eee9c8c383fc3f950374a3b28
Diogo de Andrade <diogo.g.andrade@gmail.com>
2021-04-22 15:47:13 +0100

    Aula de 22/04/2021

---
+++ Assets/Scripts/Ant.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : MonoBehaviour
+{
+    [SerializeField]
+    private float       moveDir = -1.0f;
+    [SerializeField]
+    private float       moveSpeed = 100.0f;
+    private Rigidbody2D rb;
+    [SerializeField]    
+    private Transform   groundCheckObject;
+    [SerializeField]    
+    private Transform   wallCheckObject;
+    [SerializeField]
+    private float       groundCheckRadius = 3.0f;
+    [SerializeField]
+    private LayerMask   groundCheckLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D groundCollider = Physics2D.OverlapCircle(groundCheckObject.position, groundCheckRadius, groundCheckLayer);
+
+        bool hasGround = (groundCollider != null);
+
+        Collider2D wallCollider = Physics2D.OverlapCircle(wallCheckObject.position, groundCheckRadius, groundCheckLayer);
+
+        bool hasWall = (wallCollider != null);
+
+        if ((!hasGround) || (hasWall))
+        {
+            moveDir = -moveDir;
+        }
+
+        Vector2 currentVelocity = rb.velocity;
+
+        currentVelocity.x = moveDir * moveSpeed;
+
+        rb.velocity = currentVelocity;
+
+        if (currentVelocity.x < -0.1)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation.y = 0;
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+        else if (currentVelocity.x > 0.1)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation.y = 180;
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.TakeDamage(1);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheckObject != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(groundCheckObject.position, groundCheckRadius);
+        }
+        if (wallCheckObject != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawSphere(wallCheckObject.position, groundCheckRadius);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,25 +3,49 @@
 public class Player : MonoBehaviour
 {
     [SerializeField]
+    private int         maxHearts = 3;
+    [SerializeField]
     private float       moveSpeed = 20.0f;
     [SerializeField]
     private float       jumpSpeed = 100.0f;
     [SerializeField]
+    private float       maxJumpTime = 0.1f;
+    [SerializeField]
     private int         maxJumps = 1;
+    [SerializeField]
+    private int         jumpGravityStart = 1;
+    [SerializeField]
+    Collider2D          groundCollider;
+    [SerializeField]
+    Collider2D          airCollider;
     [SerializeField]
     private Transform   groundCheckObject;
     [SerializeField]
     private float       groundCheckRadius = 3.0f;
     [SerializeField]
     private LayerMask   groundCheckLayer;
+    [SerializeField]
+    private float       invulnerabilityDuration = 2.0f;
+    [SerializeField]
+    private float       blinkDuration = 0.2f;
 
-    private float       hAxis;
-    private Rigidbody2D rb;
-    private int         nJumps;
+    private float           hAxis;
+    private Rigidbody2D     rb;
+    private SpriteRenderer  spriteRenderer;
+    private Animator        animator;
+    private int             nJumps;
+    private float           timeOfJump;
+    private int             hearts;
+    private float           invulnerabilityTimer = 0;
+    private float           blinkTimer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        hearts = maxHearts;
     }
 
     void FixedUpdate()
@@ -42,15 +66,86 @@ public class Player : MonoBehaviour
             nJumps = maxJumps;
         }
 
+        Vector2 currentVelocity = rb.velocity;
+
         if ((Input.GetButtonDown("Jump")) && (nJumps > 0))
         {
-            Vector2 currentVelocity = rb.velocity;
-
             currentVelocity.y = jumpSpeed;
 
             rb.velocity = currentVelocity;
 
             nJumps--;
+
+            rb.gravityScale = jumpGravityStart;
+
+            timeOfJump = Time.time;
+        }
+        else 
+        {
+            float elapsedTimeSinceJump = Time.time - timeOfJump;
+            if ((Input.GetButton("Jump")) && (elapsedTimeSinceJump < maxJumpTime))
+            {
+                rb.gravityScale = jumpGravityStart;
+            }
+            else
+            {
+                rb.gravityScale = 5.0f;
+            }
+        }
+
+        if (currentVelocity.x < -0.1)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation.y = 180;
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+        else if (currentVelocity.x > 0.1)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation.y = 0;
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+
+        animator.SetFloat("AbsSpeedX", Mathf.Abs(currentVelocity.x));
+        animator.SetFloat("SpeedY", currentVelocity.y);
+        animator.SetBool("OnGround", isGround);
+
+        groundCollider.enabled = isGround;
+        airCollider.enabled = !isGround;
+
+        if (invulnerabilityTimer > 0)
+        {
+            invulnerabilityTimer = invulnerabilityTimer - Time.deltaTime;
+
+            blinkTimer = blinkTimer - Time.deltaTime;
+            if (blinkTimer <= 0)
+            {
+                spriteRenderer.enabled = !spriteRenderer.enabled;
+
+                blinkTimer = blinkDuration;
+            }
+        }
+        else
+        {
+            spriteRenderer.enabled = true;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (invulnerabilityTimer > 0) return;
+        if (hearts <= 0) return;
+
+        hearts = hearts - damage;
+
+        if (hearts == 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            invulnerabilityTimer = invulnerabilityDuration;
+            blinkTimer = blinkDuration;
         }
     }
 
